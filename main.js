36a7f9e6ac68fab9b961c5f56307ffaffd7afe8b
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2015-02-25 11:25:32 +0900

    PIN発行まで

--- main.js
+++ main.js
@@ -31,11 +31,62 @@ Twitter.prototype.request_token = function(){
   };
   $.ajax(options)
   .done(function(data){
+    var param = {};
+    var array = data.split('&');
+    $.each(array, function(index, item) {
+      var keyval = item.split('=');
+      param[keyval[0]] = keyval[1];
+    });
+    console.log(param);
+
+    oauth_token_glb = param['oauth_token'];
+    $('#token').attr('value', param['oauth_token']);
+    $('a#link').attr('href', 'https://twitter.com/oauth/authorize?oauth_token=' + param['oauth_token']);
+  });
+}
+
+Twitter.prototype.send_pin = function(){
+  var accessor = {
+      consumerSecret: this.consumerSecret,
+      tokenSecret: this.tokenSecret
+  };
+
+  var message = {
+      method: 'GET',
+      action: 'https://api.twitter.com/oauth/access_token',
+      header: {
+        Authorization: 'OAuth'
+      },
+      parameters: {
+          oauth_version: "1.0",
+          oauth_verifier: $('#pin').val(),
+          oauth_signature_method: "HMAC-SHA1",
+          oauth_consumer_key: this.consumerKey,
+          oauth_token: oath_token_glb
+      }
+  };
+
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+
+  var target = OAuth.addToURL(message.action, message.parameters);
+
+  var options = {
+      type: message.method,
+      url: target
+  };
+  $.ajax(options)
+  .done(function(data){
+    console.log('OK');
     console.log(data);
+  })
+  .fail(function(){
+    console.log('fail');
   });
 }
 
 var twitter = new Twitter();
+var oauth_token_glb;
 
 function update(data){
     for( var i = 0; i < data.length; i++ ) {
@@ -43,6 +94,10 @@ function update(data){
     }
 }
 
-$(function(){
-    twitter.request_token();
+document.addEventListener('DOMContentLoaded', function() {
+    var make = document.getElementById('make');
+    link.addEventListener('click', function() {
+      console.log(oauth_token_glb);
+      twitter.request_token();
+    });
 });
